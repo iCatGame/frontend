8c85eab8bd22d35f05b4fa5929b9b468e74b2b7e
kongtaoxing <xyf8658@163.com>
2023-05-28 23:51:10 +0800

    modify polyfills

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,7 +1,7 @@
-import { Buffer } from "buffer";
+import { Buffer } from 'buffer';
 
-window.global = window.global || window;
-window.Buffer = window.Buffer || Buffer;
-window.process = window.process || { env: {} }; // Minimal process polyfill
+window.global = window.global ?? window;
+window.Buffer = window.Buffer ?? Buffer;
+window.process = window.process ?? { env: {} }; // Minimal process polyfill
 
-export default {};
+export {};
