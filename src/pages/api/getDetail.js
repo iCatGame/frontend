fc9fb6764b121140967dfa1a0088d0339abdb40b
Ye.bjtu <2876650697@qq.com>
2023-08-06 14:30:33 +0800

    提交

--- src/pages/api/getDetail.js
+++ src/pages/api/getDetail.js
@@ -4,6 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
+
 const handler = async (req, res) => {
   const { method } = req;
   switch (method) {
